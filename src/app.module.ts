12a0517875018711557ec6546561e4982bde59dd
ekuritza <elkuritza@gmail.com>
2022-01-03 14:27:38 -0300

    fix:set the string mongo_db to an enviroment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ProductsModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
